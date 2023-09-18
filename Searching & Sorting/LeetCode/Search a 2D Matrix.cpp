bc105c7a3a1688cccf306e44e10e920e4b2e08eb
Surya <82582201+SuryaCS719@users.noreply.github.com>
2023-09-18 22:56:35 +0530

    Create Search a 2D Matrix.cpp

---
+++ Searching & Sorting/LeetCode/Search a 2D Matrix.cpp
@@ -0,0 +1,36 @@
+//TC: O(log m*n) -> Binary search on 2D array
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int row = matrix.size();
+        int col = matrix[0].size();
+        int n = row * col; // Total no.of cells in a matrix
+
+        int s = 0;
+        int e = n - 1;
+
+        while(s <= e){
+            int mid = s + (e - s)/2;
+
+            // 2D to 1D Conversion Formula:
+            int rowIndex = mid / col;
+            int colIndex = mid % col;
+            int currNumber = matrix[rowIndex][colIndex];
+
+            if(currNumber == target)
+                return true;
+            else if(target > currNumber)
+                s = mid + 1;
+            else
+                e = mid - 1;
+        }
+        return false;
+    }
+};
+
+/*
+Conversion Formulas:
+2D to 1D: c * i + j 
+1D to 2D: i = currIndex / c ; j = currIndex % c
+where c = total no.of cols, i = rowIndex, j = colIndex.
+*/
