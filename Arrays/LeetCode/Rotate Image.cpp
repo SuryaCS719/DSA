5c842a026f54241b28e5c672c994735f8eef7871
Surya <82582201+SuryaCS719@users.noreply.github.com>
2023-09-18 20:49:18 +0530

    Update Rotate Image.cpp

--- Arrays/LeetCode/Rotate Image.cpp
+++ Arrays/LeetCode/Rotate Image.cpp
@@ -1,1 +1,30 @@
+// Approach: Transpose the matrix and then reverse the elements:
+class Solution {
+public:
+    // Reverse function
+    void reverseVector(vector<int>& arr){
+        int n = arr.size();
+        int low = 0;
+        int high = n-1;
+        while(low <= high){
+            swap(arr[low], arr[high]);
+            low++;
+            high--;
+        }
+    }
 
+    void rotate(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+        for(int i=0; i<n; i++){
+            for(int j=i; j<matrix[i].size(); j++){
+                swap(matrix[i][j], matrix[j][i]);
+            }
+        }
+        // transpose done, now reverse all the elements in each row
+        for(int i=0; i<n; i++){
+            // Reverse function can be used instead of writing our own function to reverse.
+            // reverse(matrix[i].begin(), matrix[i].end());
+            reverseVector(matrix[i]);
+        }
+    }
+};
