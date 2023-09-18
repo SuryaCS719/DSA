136f6b60ad69ee00b01aa35601b94d1c23642389
Surya <82582201+SuryaCS719@users.noreply.github.com>
2023-09-18 10:10:05 +0530

    Create Find Missing Element in a sorted array.cpp

---
+++ Searching & Sorting/Find Missing Element in a sorted array.cpp
@@ -0,0 +1,34 @@
+// 5. Find missing element in a sorted array
+
+int missingElement(int arr[], int n){
+    int start = 0;
+    int end = n-1;
+    int currAns = -1;
+
+    while(start <= end){
+        int mid = start + (end-start)/2;
+        int difference = arr[mid] - mid;
+
+        if(difference == 1){
+            start = mid + 1;
+        }
+        else if(difference == 2){
+            // currAns = mid; 
+            currAns = arr[mid];
+            end = mid - 1;
+        }
+    }
+
+    if(currAns == -1)
+        return n + 1;
+
+    // return currAns + 1;
+    return currAns - 1;
+}
+
+int main(){
+    int arr[] = {1,2,3,4,6,7,8,9};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    int ans = missingElement(arr, n);
+    cout << "Missing element is " << ans;
+}
