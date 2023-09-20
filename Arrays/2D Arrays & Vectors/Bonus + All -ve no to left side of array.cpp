7f4838ba1db597f0f54d951fab8c84c9e9f6e51b
Surya <82582201+SuryaCS719@users.noreply.github.com>
2023-09-20 20:45:04 +0530

    Update Bonus + All -ve no to left side of array.cpp

--- Arrays/2D Arrays & Vectors/Bonus + All -ve no to left side of array.cpp
+++ Arrays/2D Arrays & Vectors/Bonus + All -ve no to left side of array.cpp
@@ -51,30 +51,3 @@ int main(){
         cout << arr[i] << " "; 
     }
 }
-
-
-// Bonus Question: Sort 0's and 1's of an array  
-// using DutchNationalFlagAlgo
-
-void sortZeroOne(int *arr, int n){
-    int low=0, high=n-1;
-    while(low<high){
-        if(arr[low] == 0)
-            low++;
-        else if(arr[high] == 1)
-            high--;
-        else    
-            swap(arr[low], arr[high]);
-    }
-}
-
-int main(){
-    int arr[] = {0,0,0,1,0,0};
-    int n = sizeof(arr)/sizeof(arr[0]);
-    // sort(arr, arr + n);
-    sortZeroOne(arr, n); 
-
-    for(int i = 0; i<n; i++){
-        cout << arr[i] << " "; 
-    }
-}
