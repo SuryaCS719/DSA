2f998e256b20fb3cc22211b73a4f9967fb3bbb65
Surya <82582201+SuryaCS719@users.noreply.github.com>
2023-09-27 10:17:28 +0530

    Update Dividing of 2 numbers using Binary Search.cpp

--- Searching & Sorting/Dividing of 2 numbers using Binary Search.cpp
+++ Searching & Sorting/Dividing of 2 numbers using Binary Search.cpp
@@ -38,3 +38,56 @@ int main(){
 }
 
 // 10.2 Dividing of 2 numbers and to produce the o/p with n precision values.
+
+#include <iostream>
+#include <assert.h>
+using namespace std;
+
+int getQuotient(int divisor, int divident){
+  int s = 0;
+  int e = divident;
+  int ans = -1;
+  while(s <= e){
+    int mid = s + (e-s)/2;
+    if(mid*divisor > divident)
+      e = mid - 1;
+    else if(mid*divisor < divident){
+      ans = mid;
+      s = mid + 1; 
+    }
+  }
+  return ans;
+}
+
+double preciseQuotient(int divisor, int divident, int precision){
+  double integerAns = getQuotient(divisor, divident);
+  double step = 0.1;
+  for(int i = 0; i < precision; i++){
+    double j = integerAns;
+    while(j * divisor <= divident){
+      integerAns = j;
+      j += step;
+    }
+    step /= 10;
+  }
+  return integerAns;
+}
+
+int main(){
+  int divisor = 0; 
+  int divident = 51; // if divident = 0 then o/p gives 0 by dft.
+  int precision = 4;
+
+   //sp. case if divisor == 0
+   if(divisor == 0)
+    assert(divisor != 0); // assert activates if the condition is false.
+
+  double finalAns = preciseQuotient(abs(divisor) ,abs(divident), precision);
+  
+  if((divisor > 0 && divident < 0) || (divisor < 0 && divident > 0))
+    cout << 0 - finalAns << endl;
+  else 
+    cout << finalAns << endl;
+
+  return 0;
+}
