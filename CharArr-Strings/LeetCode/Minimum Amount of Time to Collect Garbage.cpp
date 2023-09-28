7f9329bcb47e020d7be5734b7d8828e0c216d442
Surya <82582201+SuryaCS719@users.noreply.github.com>
2023-09-28 14:49:21 +0530

    Create Minimum Amount of Time to Collect Garbage.cpp

---
+++ CharArr-Strings/LeetCode/Minimum Amount of Time to Collect Garbage.cpp
@@ -0,0 +1,63 @@
+class Solution {
+public:
+    int garbageCollection(vector<string>& garbage, vector<int>& travel) {
+        // int pickUpTimeM = 0; 
+        // int pickUpTimeP = 0;
+        // int pickUpTimeG = 0;
+
+        // int travelTimeM = 0;
+        // int travelTimeP = 0;
+        // int travelTimeG = 0; 
+
+        // int lastHouseM = 0;
+        // int lastHouseP = 0;
+        // int lastHouseG = 0;
+
+        int pickUpTimeM = 0, pickUpTimeP = 0, pickUpTimeG = 0;
+        int travelTimeM = 0, travelTimeP = 0, travelTimeG = 0;
+        int lastHouseM = 0, lastHouseP = 0, lastHouseG = 0;
+
+        
+        // Calculate pickup time and travel time
+        for(int i=0; i < garbage.size(); i++){
+            string current = garbage[i];
+
+            for(int j=0; j < current.length(); j++){
+                char ch = current[j];
+
+                if(ch == 'M'){
+                    pickUpTimeM++;
+                    lastHouseM = i;
+                }
+
+                else if(ch == 'P'){
+                    pickUpTimeP++;
+                    lastHouseP = i;
+                }
+
+                else if(ch == 'G'){
+                    pickUpTimeG++;
+                    lastHouseG = i;
+                }
+            }
+        }
+
+        //calc travel time
+        for(int i=0; i < lastHouseM; i++){
+            travelTimeM += travel[i];
+        }
+
+        for(int i=0; i < lastHouseP; i++){
+            travelTimeP += travel[i];
+        }
+
+        for(int i=0; i < lastHouseG; i++){
+            travelTimeG += travel[i];
+        }
+
+        // To get the final answer:
+        int totalTime = (pickUpTimeM + travelTimeM) + (pickUpTimeP + travelTimeP) + (pickUpTimeG + travelTimeG);
+
+        return totalTime;
+    }
+};
