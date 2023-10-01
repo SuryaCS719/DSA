dd3cec9676faa594fd526b50a355d112dc05ef57
Surya <82582201+SuryaCS719@users.noreply.github.com>
2023-10-01 10:48:18 +0530

    Create [GFG] Book Allocation Problem.cpp

---
+++ Searching & Sorting/LeetCode/[GFG] Book Allocation Problem.cpp
@@ -0,0 +1,45 @@
+// GFG: Allocate minimum number of pages
+
+class Solution 
+{
+    public:
+    
+    bool isPossibleSolution(int arr[], int n, int m, int sol){
+        int pageSum = 0;
+        int numberOfAllocations = 1;
+        for(int i = 0; i < n; i++){
+            if(arr[i] > sol)
+                return false;
+            if(pageSum + arr[i] > sol){
+                numberOfAllocations++;
+                pageSum = arr[i];
+                if(numberOfAllocations > m)
+                    return false;
+            }
+            else
+                pageSum += arr[i];
+        }
+        return true;
+    }
+    
+    //Function to find minimum number of pages.
+    int findPages(int A[], int N, int M) 
+    {
+        //Using binary search we can define the search space
+        int ans = -1;
+        if(M > N) return ans; // since each student must be allocated atleast 1 book
+        int start = 0;
+        int end = accumulate(A, A+N, 0); // (startAddr, endAddr, sumStartPoint)
+        
+        while(start <= end){
+            int mid = start + ((end-start) >> 1);
+            if(isPossibleSolution(A, N, M, mid)){
+                ans = mid;
+                end = mid - 1;
+            }
+            else
+                start = mid + 1;
+        }
+        return ans;
+    }
+};
