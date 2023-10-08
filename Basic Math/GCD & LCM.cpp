c3ea54373b791dc0e79af43fe99b15ba605e0510
Surya <82582201+SuryaCS719@users.noreply.github.com>
2023-10-08 13:58:24 +0530

    Update GCD & LCM.cpp

--- Basic Math/GCD & LCM.cpp
+++ Basic Math/GCD & LCM.cpp
@@ -1,14 +1,15 @@
 // GFG - GCD of two numbers
 
-// Method 1: Euclid's algorithm
+// Method 1: Euclid's algorithm ; TC: O(log(min(A,B))) ; SC: O(1)
 
 class Solution
 {
 	public:
     int gcd(int A, int B) 
 	{
-	    if(A == 0) return B;
-	    if(B == 0) return A;
+	    // Not needed cuz of the return statement condition.
+	    // if(A == 0) return B;
+	    // if(B == 0) return A;
 	    
 	    while(A > 0 && B > 0){
 	        if(A > B){
