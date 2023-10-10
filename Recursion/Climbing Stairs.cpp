e7db943a2df1494ff3423349603b68c9b6b21e34
Surya <82582201+SuryaCS719@users.noreply.github.com>
2023-10-11 00:38:29 +0530

    Update Climbing Stairs.cpp

--- Recursion/Climbing Stairs.cpp
+++ Recursion/Climbing Stairs.cpp
@@ -20,3 +20,20 @@ public:
         return climbStairs(n-1) + climbStairs(n-2);
     }
 };
+
+
+// iterative approach - same as fibonacci logic:
+class Solution {
+public:
+    int climbStairs(int n) {
+        int term1 = 0;
+        int term2 = 1;
+        int term3;
+        for(int i=0; i<n; i++){
+            term3 = term1 + term2;
+            term1 = term2;
+            term2 = term3;
+        }
+        return term3;
+    }
+};
