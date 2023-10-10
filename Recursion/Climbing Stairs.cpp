54149a32b4ddbbba1c7994a7cb5661622c62d25e
Surya <82582201+SuryaCS719@users.noreply.github.com>
2023-10-11 00:29:27 +0530

    Update Climbing Stairs.cpp

--- Recursion/Climbing Stairs.cpp
+++ Recursion/Climbing Stairs.cpp
@@ -3,8 +3,20 @@
 class Solution {
 public:
     int climbStairs(int n) {
-        if(n==0 || n==1)
+        if(n==0 || n==1) // n = 0 ; 1 way to reach by jumping in the same position
             return 1;
+        return climbStairs(n-1) + climbStairs(n-2);
+    }
+};
+
+// given -> 1<= n <= 45 ; neednt include base case where n = 0
+class Solution {
+public:
+    int climbStairs(int n) {
+        if(n == 1)
+            return 1;
+        if(n == 2)
+            return 2;
         return climbStairs(n-1) + climbStairs(n-2);
     }
 };
