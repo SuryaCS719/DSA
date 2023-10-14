466d391530149241fbd7f0908ad5944d2c67f4fe
Surya <82582201+SuryaCS719@users.noreply.github.com>
2023-10-14 12:03:37 +0530

    Create Maximize The Cut Segments.cpp

---
+++ Recursion/Maximize The Cut Segments.cpp
@@ -0,0 +1,19 @@
+// Correct solution but will TLE as this is supposed to be solved using DP.
+// Pattern: Exploring all possible ways
+class Solution
+{
+    public:
+    //Function to find the maximum number of cuts.
+    int maximizeTheCuts(int n, int x, int y, int z)
+    {
+        if(n == 0)
+            return 0;
+        if(n < 0)
+            return INT_MIN;
+        
+        int ans1 = 1 + maximizeTheCuts(n-x, x, y, z);
+        int ans2 = 1 + maximizeTheCuts(n-y, x, y, z);
+        int ans3 = 1 + maximizeTheCuts(n-z, x, y, z);
+        return max(ans1, max(ans2, ans3));
+    }
+};
