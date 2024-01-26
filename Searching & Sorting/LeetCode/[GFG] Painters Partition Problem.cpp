3a1e000fe7b80532bd5e8761b27c382fd833b2d7
Surya <82582201+SuryaCS719@users.noreply.github.com>
2024-01-26 22:03:13 +0530

    Create [GFG] Painters Partition Problem.cpp

---
+++ Searching & Sorting/LeetCode/[GFG] Painters Partition Problem.cpp
@@ -0,0 +1,44 @@
+class Solution
+{
+  public:
+  
+    bool isPossibleSolution(int a[], int n, int k, long long mid){
+        long long timeSum = 0;
+        int count = 1;
+        for(int i = 0; i < n; i++){
+            if(a[i] > mid) return false;
+            if(timeSum + a[i] > mid){
+                count++;
+                timeSum = a[i];
+                if(count > k) return false;
+            }
+            else
+                timeSum += a[i];
+        }
+        return true;
+    }
+  
+    long long minTime(int arr[], int n, int k)
+    {
+        // return minimum time
+        long long start = 0;
+        // long long end = accumulate(arr, arr + n, 0);
+        long long end = 0;
+        for(int i = 0; i < n; i++){
+            end += arr[i];
+        }
+        
+        long long ans = -1;
+        
+        while(start <= end){
+            long long mid = start + (end - start)/2;
+            if(isPossibleSolution(arr, n, k, mid)){
+                ans = mid;
+                end = mid - 1;
+            }
+            else
+                start = mid + 1;
+        }
+        return ans;
+    }
+};
