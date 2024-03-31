230c394012672eaa40e0b73026fe6be51ee813d9
Surya <82582201+SuryaCS719@users.noreply.github.com>
2024-03-31 21:02:42 +0530

    Update [GFG] Quick Sort.cpp

--- Backtracking & DnC/[GFG] Quick Sort.cpp
+++ Backtracking & DnC/[GFG] Quick Sort.cpp
@@ -10,15 +10,17 @@ class Solution
         int pivot = high;
         int i = low - 1;
         int j = low;
-        
-        while(j < pivot){
+
+        // pivot == end i.e can use either of them to run the while loop.
+        while(j < pivot){ 
             if(arr[j] < arr[pivot]){
                 i++;
                 swap(arr[i], arr[j]);
             }
             j++;
         }
-        i++; // after this stmt, i is the right position for the pivot element
+        i++; 
+        // now i is the correct position for pivot element
         swap(arr[i], arr[pivot]);
         
         quickSort(arr, low, i-1);
@@ -29,7 +31,9 @@ class Solution
     public:
     int partition (int arr[], int low, int high)
     {
-       // Your code here
        // WON'T USE THIS. AS WE ARE IMPLEMENTING IN A DIFFERENT WAY.
     }
 };
+
+
+
