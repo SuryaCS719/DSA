782e4acb54f3862090e053ad56fdb67002cb384a
Surya <82582201+SuryaCS719@users.noreply.github.com>
2024-03-31 20:52:12 +0530

    Update [Custom] Merge sort.cpp

--- Backtracking & DnC/[Custom] Merge sort.cpp
+++ Backtracking & DnC/[Custom] Merge sort.cpp
@@ -1,91 +1,76 @@
 #include <iostream>
 using namespace std;
 
-void merge(int* arr, int s, int e){
-    
-    int mid = s + (e-s)/2;
-    int lenLeft = mid - s + 1;
-    int lenRight = e - mid;
+void merge(int arr[], int s, int e, int mid){
+  // int mid = s + (e - s)/2;
+  int lenLeft = mid - s + 1;
+  int lenRight = e - mid;
 
-    int* leftArr = new int[lenLeft];
-    int* rightArr = new int[lenRight]; 
+  // create left and right arr dynamic allocation heap memory
+  int* left_arr = new int[lenLeft];
+  int* right_arr = new int[lenRight];
 
-    // copy elements from original arr's left part to leftArr
-    int k = s;
-    for(int i = 0; i < lenLeft; i++){
-        leftArr[i] = arr[k];
-        k++;
+  int k = s;
+  // copy values from original array to left_arr
+  for(int i = 0; i < lenLeft; i++){
+    left_arr[i] = arr[k];
+    k++;
+  }
+
+  k = mid + 1; 
+  // copy values from original array to right_arr
+  for(int i = 0; i < lenRight; i++)
+    right_arr[i] = arr[k++];
+
+  // Actual Merge logic
+  int i = 0; // leftIndex
+  int j = 0; // rightIndex
+  int mainArrayIndex = s; // Important. 
+
+  while(i < lenLeft && j < lenRight){
+    if(left_arr[i] < right_arr[j]){
+      arr[mainArrayIndex] = left_arr[i];
+      mainArrayIndex++;
+      i++;
     }
-
-    // copy elements from original arr's right part to rightArr
-    k = mid + 1;
-    for(int i = 0; i < lenRight; i++){
-        rightArr[i] = arr[k];
-        k++;
+    else{
+      arr[mainArrayIndex] = right_arr[j];
+      mainArrayIndex++;
+      j++;
     }
+  }
 
-    int leftArrIndex = 0;
-    int rightArrIndex = 0;
-    int mainArrIndex = s;
+  while(i < lenLeft){
+    arr[mainArrayIndex] = left_arr[i];
+    mainArrayIndex++;
+    i++;
+  }
 
-    while(leftArrIndex < lenLeft && rightArrIndex < lenRight){
-        if(leftArr[leftArrIndex] < rightArr[rightArrIndex]){
-            arr[mainArrIndex] = leftArr[leftArrIndex];
-            mainArrIndex++;
-            leftArrIndex++;
-        }
-        else{
-            arr[mainArrIndex] = rightArr[rightArrIndex];
-            mainArrIndex++;
-            rightArrIndex++;
-        }
-    }
-
-    while(leftArrIndex < lenLeft){
-        arr[mainArrIndex] = leftArr[leftArrIndex];
-        mainArrIndex++;
-        leftArrIndex++;
-    }
-
-    while(rightArrIndex < lenRight){
-        arr[mainArrIndex] = rightArr[rightArrIndex];
-        mainArrIndex++;
-        rightArrIndex++;
-    }
-
-    // de-allocate the heap memory ; best practice
-    delete[] leftArr;
-    delete[] rightArr;
+  while(j < lenRight){
+    arr[mainArrayIndex] = right_arr[j];
+    mainArrayIndex++;
+    j++;
+  }
+  // Deallocation of Heap memory
+  delete[] left_arr;
+  delete[] right_arr;
 }
 
-void mergeSort(int* arr, int s, int e){
-    if(s > e)
-        return;
-    if(s == e)
-        return; 
-    int mid = s + (e-s)/2;
-    mergeSort(arr, s, mid); // left call
-    mergeSort(arr, mid+1, e); // right call 
-    merge(arr, s, e);     // merge 2 sorted arr.
+void mergeSort(int arr[], int s, int e){
+  if(s >= e) return;
+  int mid = s + (e - s)/2; 
+  mergeSort(arr, s, mid); //left_arr
+  mergeSort(arr, mid + 1, e); //right_arr
+  merge(arr, s, e, mid); // merge 2 sorted arr
 }
 
-int main(){
-    int arr[] = {2,1,6,9,4,5};
-    int size = sizeof(arr)/sizeof(int);
-    cout << "Before Merge sort: " << endl;
-    for(auto i: arr)
-        cout << i << " ";
-
-    mergeSort(arr, 0, size-1);
-
-    cout << endl << "After Merge sort: " << endl;
-    for(auto i: arr)
-        cout << i << " ";
+int main() {
+  int arr[] = {2, 1, 6, 9, 4, 5};
+  int size = 6; 
+  int s = 0;
+  int e = size - 1;
+  mergeSort(arr, s, e);
+  for(auto it : arr)
+    cout << it << " ";
+  return 0;
 }
-
-/*
-Before Merge sort: 
-2 1 6 9 4 5 
-After Merge sort: 
-1 2 4 5 6 9 
-*/
