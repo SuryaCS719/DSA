c37af6f146f015790b88e5e80a097956ee002b7a
Surya <82582201+SuryaCS719@users.noreply.github.com>
2024-04-29 13:53:00 +0530

    Update middle element of a stack.cpp

--- Stacks/middle element of a stack.cpp
+++ Stacks/middle element of a stack.cpp
@@ -1,3 +1,5 @@
+// Approach 1: Using system stack via recursion
+
 // Using VOID type of function
 #include <iostream>
 #include <stack>
@@ -99,3 +101,62 @@ int main() {
 
   return 0;
 }
+
+
+
+
+// Approach 2: Using user defined stack 
+
+#include <iostream>
+#include <stack>
+using namespace std;
+
+int getMiddleElement(stack<int> &st) {
+  stack<int> st2;
+  int pos = 0;
+  int size = st.size();
+  // check for odd or even sized stack
+  if(size & 1)
+      pos = size / 2 + 1;
+  else pos = size / 2;
+
+  while(pos > 1) {
+    pos--;
+    int TOS_st = st.top();
+    st.pop();
+    st2.push(TOS_st);
+  }
+  // atp pos == 1 i.e. TOS of st is the middle element 
+  int ans = st.top();
+  
+  // now inserting the popped elements from st to st2 back again
+  while(!st2.empty()) {
+    // int TOS_st2 = st2.top();
+    st.push(st2.top());
+    st2.pop();
+  }
+
+  return ans;
+}
+
+int main() {
+  stack<int> st;
+  st.push(10);
+  st.push(20);
+  st.push(30);
+  st.push(40);
+  st.push(50);
+  // st.push(60);
+
+  if(st.empty()){
+    cout << "stack underflow";
+    return 0;
+  }
+
+  int mid = getMiddleElement(st);
+  cout << "Middle element is: " << mid << endl;
+  
+  return 0;
+}
+
+
