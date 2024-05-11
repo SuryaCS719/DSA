b5bf63e8d8f89f1bccb1da1cbf3487a298422a71
Surya <82582201+SuryaCS719@users.noreply.github.com>
2024-05-11 13:27:09 +0530

    Update Construct Binary Tree from Preorder and Inorder Traversal.cpp

--- Binary & Generic Trees/Construct Binary Tree from Preorder and Inorder Traversal.cpp
+++ Binary & Generic Trees/Construct Binary Tree from Preorder and Inorder Traversal.cpp
@@ -16,7 +16,7 @@ public:
 };
 
 void printLevelWise(TreeNode* root){
-  // if(root == NULL) return;
+  if(root == NULL) return;
   queue<TreeNode*> q;
   q.push(root);
   q.push(NULL);
@@ -70,6 +70,100 @@ int main() {
   int inOrdeStart = 0;
   int inOrderEnd = 5;
   TreeNode* root = constructTreeFromPreAndInorderTraversal(preOrder, preOrderIndex, inOrder, inOrdeStart, inOrderEnd, size);
+  printLevelWise(root);
+  return 0;
+}
+
+
+
+// using map to find element in inOrder array
+
+#include <iostream>
+#include <queue>
+#include <map>
+using namespace std;
+
+class TreeNode {
+public:
+  int val;
+  TreeNode* left;
+  TreeNode* right;
+
+  TreeNode(int data) {
+    val = data;
+    left = NULL;
+    right = NULL;
+  } 
+};
+
+void printLevelWise(TreeNode* root){
+  // if(root == NULL) return;
+  queue<TreeNode*> q;
+  q.push(root);
+  q.push(NULL);
+  while(!q.empty()) {
+    TreeNode* front = q.front();
+    q.pop();
+    if(front == NULL) {
+      cout << endl;
+      if(!q.empty())
+        q.push(NULL);
+    }
+    else {
+      cout << front->val << " ";
+      if(front->left != NULL)
+        q.push(front->left);
+      if(front->right != NULL)
+        q.push(front->right);
+    }
+  }
+}
+
+// int searchInorder(int *inOrder, int target, int size) {
+//   for(int i = 0; i < size; i++) 
+//     if(inOrder[i] == target)
+//       return i;
+//   return -1;
+// }
+
+void createMap (int *inOrder, int size, map<int, int> &valueToIndexMap) {
+  for(int i = 0; i < size; i++) {
+    int elementOfInorder = inOrder[i];
+    int index = i;
+    valueToIndexMap[elementOfInorder] = index;
+  }
+}
+
+TreeNode* constructTreeFromPreAndInorderTraversal(int *preOrder, int &preOrderIndex, int *inOrder, int inOrdeStart, int inOrderEnd, int size, 
+map<int, int> &valueToIndexMap) {
+  // base case: out_of_bound conditions
+  if(preOrderIndex >= size || inOrdeStart > inOrderEnd)
+    return NULL;
+  // solve for 1 case
+  int preOrderFirstElement = preOrder[preOrderIndex];
+  preOrderIndex++;
+  TreeNode* root = new TreeNode(preOrderFirstElement);
+  // finding root in inOrder
+  int position = valueToIndexMap[preOrderFirstElement]; // -> O(1)
+  // int position = searchInorder(inOrder, preOrderFirstElement, size); //-> O(n)
+  //recursion handles the rest
+  root->left = constructTreeFromPreAndInorderTraversal(preOrder, preOrderIndex, inOrder, inOrdeStart, position - 1, size, valueToIndexMap);
+  root->right = constructTreeFromPreAndInorderTraversal(preOrder, preOrderIndex, inOrder, position + 1, inOrderEnd, size, valueToIndexMap);
+  return root;
+}
+
+int main() {
+  int inOrder[] = {10, 8, 6, 2, 4, 12};
+  int preOrder[] = {2, 8, 10, 6, 4, 12};
+  int size = 6;
+  int preOrderIndex = 0;
+  int inOrdeStart = 0;
+  int inOrderEnd = 5;
+
+  map<int, int> valueToIndexMap;
+  createMap(inOrder, size, valueToIndexMap);
+
+  TreeNode* root = constructTreeFromPreAndInorderTraversal(preOrder, preOrderIndex, inOrder, inOrdeStart, inOrderEnd, size, valueToIndexMap);
   printLevelWise(root);
   return 0;
 }
