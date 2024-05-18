387e74f8af8c3b906476426b5818b06136c8a284
Surya <82582201+SuryaCS719@users.noreply.github.com>
2024-05-19 00:11:10 +0530

    Update Kth Shift array element.cpp

--- Arrays/Level-2/Kth Shift array element.cpp
+++ Arrays/Level-2/Kth Shift array element.cpp
@@ -26,3 +26,55 @@ int main(){
         cout << arr[i] << " ";
     }
 }
+
+
+
+
+
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+// shifting elements by k index to right.
+void rotateR(int *arr, int n, int k) {
+  // store the k elements of the array from the end
+  vector<int> temp(k);
+  for(int i = n - k, j = 0; i < n; i++, j++)
+    temp[j] = arr[i];
+  
+  for(int i = n - 1; i >= k; i--)
+    arr[i] = arr[i - k];
+  
+  for(int i = 0; i < k; i++)
+    arr[i] = temp[i];
+
+}
+
+// shifting elements by k index to left.
+// temp = 1,2,3
+// output = [4, 5, 6, 7, 1, 2, 3]
+void rotateL(int *arr, int n, int k) {
+  // store the k elements of the array from the end
+  vector<int> temp(k);
+  for(int i = 0; i < k; i++)
+    temp[i] = arr[i];
+  
+  for(int i = 0; i <= k; i++) {
+    arr[i] = arr[i + k];
+  }
+
+  for(int i = n - k, j = 0; i < n; i++, j++)
+    arr[i] = temp[j];
+
+}
+
+int main() {
+  int arr[] = {1,2,3,4,5,6,7};
+  int size = 7;
+  int k = 3;
+  rotateL(arr, size, k);
+  for(auto i : arr)
+    cout << i << " ";
+  return 0;
+}
