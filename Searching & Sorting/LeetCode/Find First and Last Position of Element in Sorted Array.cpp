5167ca757b9d52cdc483d30b6554db3c30d33f43
Surya <82582201+SuryaCS719@users.noreply.github.com>
2024-08-08 20:45:51 +0530

    Update Find First and Last Position of Element in Sorted Array.cpp

--- Searching & Sorting/LeetCode/Find First and Last Position of Element in Sorted Array.cpp
+++ Searching & Sorting/LeetCode/Find First and Last Position of Element in Sorted Array.cpp
@@ -6,10 +6,15 @@ public:
         int n = nums.size();
         int startIndex = lower_bound(nums.begin(), nums.end(), target) - nums.begin();
         int endIndex = upper_bound(nums.begin(), nums.end(), target) - nums.begin();
+
+        int lastIndex = endIndex - 1;
+        // also optional part: printing total occurences of the target
+        int totalOccurence = lastIndex - startIndex + 1;
+        cout << "total: " << totalOccurence << endl;
         
-        if(startIndex < n && endIndex - 1 < n 
-            && nums[startIndex] == target && nums[endIndex - 1] == target)
-            return {startIndex, endIndex - 1};
+        if(startIndex < n && lastIndex < n 
+            && nums[startIndex] == target && nums[lastIndex] == target)
+            return {startIndex, lastIndex};
         
         return {-1, -1};
     }   
